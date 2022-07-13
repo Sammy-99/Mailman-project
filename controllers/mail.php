2072889f90af6eb9b1a963cd7e7c60466aa002a9
Samir Ahamad <samirahamad999@gmail.com>
2022-07-13 19:26:06 +0530

    image path

--- controllers/mail.php
+++ controllers/mail.php
@@ -12,29 +12,54 @@ require 'PHPMailer/src/SMTP.php';
 
 $mail = new PHPMailer(true);
 
-try {
+// try {
  
                   
-    $mail->isSMTP();                                           
-    $mail->Host       = 'smtp.gmail.com';                     
-    $mail->SMTPAuth   = true;                                   
-    $mail->Username   = 'samirahamad999@gmail.com';                    
-    $mail->Password   = 'ojsdtutobkfxayxw';                             
-    // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;  
-    $mail->SMTPSecure = "ssl";          
-    $mail->Port       = 465;    
-    // $mail->Port       = 587;                                    
+//     $mail->isSMTP();                                           
+//     $mail->Host       = 'smtp.gmail.com';                     
+//     $mail->SMTPAuth   = true;                                   
+//     $mail->Username   = 'samirahamad999@gmail.com';                    
+//     $mail->Password   = 'ojsdtutobkfxayxw';                             
+//     // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;  
+//     $mail->SMTPSecure = "ssl";          
+//     $mail->Port       = 465;    
+//     // $mail->Port       = 587;                                    
 
-    $mail->setFrom('samirahamad999@gmail.com', 'Dev Ninja Youtube');
-    $mail->addAddress('samirahamad999@gmail.com');             
+//     $mail->setFrom('samirahamad999@gmail.com', 'Dev Ninja Youtube');
+//     $mail->addAddress('samirahamad999@gmail.com');             
 
-    $mail->isHTML(true);                                 
-    $mail->Subject = 'Here is the subject'.time();
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+//     $mail->isHTML(true);                                 
+//     $mail->Subject = 'Here is the subject'.time();
+//     $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+//     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+//     $mail->send();
+//     echo 'Message has been sent';
+// } catch (Exception $e) {
+//     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+// }
+
+//$mail->isSMTP();
+$mail->SMTPDebug = 2;
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPAuth = true;
+
+// But you can comment from here
+$mail->SMTPSecure = "tls";
+$mail->Port = 587;
+$mail->CharSet = "UTF-8";
+// To here. 'cause default secure is TLS.
+
+$mail->setFrom("123456@gmail.com", "Ololoev");
+$mail->Username = "mygmail@gmail.com";
+$mail->Password = "ojsdtutobkfxayxw";
+
+$mail->Subject = "Тест";
+$mail->msgHTML("Test");
+$mail->addAddress("samirhestabit999@gmail.com", "Alex");
+
+if (!$mail->send()) {
+echo " not send ";
+} else {
+echo "mail send ";
 }
