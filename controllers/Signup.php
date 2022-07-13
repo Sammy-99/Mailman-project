697c1d94a6c2ae8531b4ea26aafc9a850e9ef47b
Samir Ahamad <samirahamad999@gmail.com>
2022-07-13 18:28:02 +0530

    image path

--- controllers/Signup.php
+++ controllers/Signup.php
@@ -36,7 +36,7 @@ class SignUp{
             {
                 // $todir = "/var/www/html/launchpadtwo/uploadedimage/";
                 // $todir = StoreUrl::$baseUrl . "uploadedimage/";
-                $todir = "../uploadedimage";
+                $todir = "../uploadedimage/";
                 $uniqueSaveName = time()."-".$_FILES['user-image']['name'] ;
                 if ( move_uploaded_file( $_FILES['user-image']['tmp_name'], $todir . $uniqueSaveName)){
                     $this->profile_image = $uniqueSaveName;
